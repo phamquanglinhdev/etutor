0f224ef35a0105979bb6af97973f6a12d83c6d64
Phamquanglinh <Linh2002gv@gmail.com>
2021-04-05 11:30:48 +0700

    add rating

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -22,5 +22,6 @@
                 class='nav-icon la la-user-alt-slash'></i> Người dùng</a></li>
 @endif
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-question'></i> Pages</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('comment') }}'><i class='nav-icon la la-question'></i> Comments</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon la la-question'></i>Các trang tĩnh</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('comment') }}'>
+        <i class='nav-icon la la-question'></i>{{?backpack_user()->role==0 ?'Đánh giá BizEnglish':'Đánh giá giáo viên '.backpack_user()->name}}</a></li>
