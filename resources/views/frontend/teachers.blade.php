4e878e8a41eb50627d33a4359981a95457c321ca
Phamquanglinh <Linh2002gv@gmail.com>
2021-05-29 17:09:16 +0700

    a

--- resources/views/frontend/teachers.blade.php
+++ resources/views/frontend/teachers.blade.php
@@ -31,23 +31,23 @@
         <div class="p-5 h1 text-origin text-center">
             {{$title}}
         </div>
-        <div class="row">
-            @foreach($data as $key => $teacher)
-                <div class="col-md-3 col-sm-6 col-12 p-3">
-                    <div class="card">
-                        <a href="{{route('teacher',['id'=>$data[$key]['id']])}}">
-                            <div class="avatar position-relative">
-                                <img class="img-fluid"
-                                     src="{{ $data[$key]['avatar'] != null ? $data[$key]['avatar'] : 'https://cloudcone.com/wp-content/uploads/2019/03/blank-avatar.jpg'}}">
-                            </div>
-                            <div class="text-dark p-2">
+        @foreach($data as $key => $teacher)
+            <a href="{{route('teacher',['id'=>$data[$key]['id']])}}">
+                <div class="row">
+                    <div class="col-md-3 col-sm-6-col-12">
+                        <img class="img-fluid"
+                             src="{{ $data[$key]['avatar'] != null ? $data[$key]['avatar'] : 'https://cloudcone.com/wp-content/uploads/2019/03/blank-avatar.jpg'}}">
+                    </div>
+                    <div class="text-dark p-2 col-md-9 col-sm-6 col-12">
+                        <div class="row">
+                            <div class="col-md-6 col-12">
                                 <b>{{$data[$key]['name']}}</b>
                             </div>
-                        </a>
+                        </div>
                     </div>
                 </div>
-            @endforeach
-        </div>
+            </a>
+        @endforeach
     </div>
     <style>
         .avatar {
