425d1cb2e50c3f44d3e21f64add66576758ee861
Phamquanglinh <Linh2002gv@gmail.com>
2021-05-29 17:00:22 +0700

    a

--- resources/views/frontend/teachers.blade.php
+++ resources/views/frontend/teachers.blade.php
@@ -1,25 +1,31 @@
 @extends('layout.app')
 @section('content')
-    <div class = "container">
-        <form action="#" method="get">
-            @csrf
-            <div class="row p-3">
-                @for($a=1;$a<=4;$a++)
-                    <div class="col-md-3 col-sm-6 col-12">
-                        <div class="font-weight-bold text-origin">Khóa học</div>
-                        @for($i=1;$i<=3;$i++)
-                            <div class="form-check-inline">
-                                <input id="data-{{$i*$a}}" type="checkbox" value="{{$i*$a}}" name="data-teach">
-                                <label for="data-{{$i*$a}}">Khóa học mẫu {{$i*$a}}</label>
-                            </div>
-                        @endfor
-                    </div>
-                @endfor
-            </div>
-            <div class="text-center">
-                <button type="submit" class="btn btn-origin">Lọc</button>
-            </div>
-        </form>
+    <div class="container ">
+        <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample"
+                aria-expanded="false" aria-controls="collapseExample">
+            Lọc giáo viên
+        </button>
+        <div class="collapse" id="collapseExample">
+            <form action="#" method="get">
+                @csrf
+                <div class="row p-3">
+                    @for($a=1;$a<=4;$a++)
+                        <div class="col-md-3 col-sm-6 col-12">
+                            <div class="font-weight-bold text-origin">Khóa học</div>
+                            @for($i=1;$i<=3;$i++)
+                                <div class="form-check-inline">
+                                    <input id="data-{{$i*$a}}" type="checkbox" value="{{$i*$a}}" name="data-teach">
+                                    <label for="data-{{$i*$a}}">Khóa học mẫu {{$i*$a}}</label>
+                                </div>
+                            @endfor
+                        </div>
+                    @endfor
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-origin">Lọc</button>
+                </div>
+            </form>
+        </div>
     </div>
     <div class="container">
         <div class="p-5 h1 text-origin text-center">
