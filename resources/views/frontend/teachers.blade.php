cb8c689d897981ade48cd38b95ed39a033d2437e
Phamquanglinh <Linh2002gv@gmail.com>
2021-05-29 17:24:42 +0700

    a

--- resources/views/frontend/teachers.blade.php
+++ resources/views/frontend/teachers.blade.php
@@ -41,11 +41,31 @@
                         </div>
                         <div class="text-dark p-2 col-md-9 col-sm-6 col-12">
                             <div class="row">
-                                <div class="col-md-6 col-12 h4">
-                                    <b>{{$data[$key]['name']}}</b>
+                                <div class="col-md-6 col-12">
+                                    <b class="h4">{{$data[$key]['name']}}</b>
+                                    <div class="w-100">
+                                        {!! $data[$key]['level']!!}
+                                    </div>
                                 </div>
-                                <div class="w-100">
-                                    {!! $data[$key]['level']!!}
+                                <div class="col-md-6 col-12">
+                                    <nav>
+                                        <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                                            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home-{{$key}}" aria-selected="true">Home</a>
+                                            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile-{{$key}}" aria-selected="false">Lịch dạy</a>
+                                            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact-{{$key}}" aria-selected="false">Tôi muốn học</a>
+                                        </div>
+                                    </nav>
+                                    <div class="tab-content" id="nav-tabContent">
+                                        <div class="tab-pane fade show active" id="nav-home-{{$key}}" role="tabpanel" aria-labelledby="nav-home-tab">
+                                            TAV
+                                        </div>
+                                        <div class="tab-pane fade" id="nav-profile-{{$key}}" role="tabpanel" aria-labelledby="nav-profile-tab">
+                                            {{$data[$key]['table']}}
+                                        </div>
+                                        <div class="tab-pane fade" id="nav-contact-{{$key}}" role="tabpanel" aria-labelledby="nav-contact-tab">
+                                            BB
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
